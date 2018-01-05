651991ea696fd2a82aaaa377bd15c71e17907817
koala-PC\koala <cnynyn@163.com>
2018-01-05 16:27:05 +0800

    已完成2018年1月5日16:27:00

--- src/com/mybatis/test/Testing.java
+++ src/com/mybatis/test/Testing.java
@@ -11,7 +11,6 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.mybatis.dao.UserDao;
 import com.mybatis.pojo.User;
 
 public class Testing {
@@ -25,7 +24,6 @@ public class Testing {
 		InputStream inputStream = Resources.getResourceAsStream(resource);
 		// 使用mybatis的类sqlSesionfactoryBuider创建工厂
 		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
 	}
 
 	@Test
@@ -41,9 +39,9 @@ public class Testing {
 	
 	@Test
 	public void t2() throws IOException {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//		String resource = "mybatis-config.xml";
+//		InputStream inputStream = Resources.getResourceAsStream(resource);
+//		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		User user = new User();
 		user.setAge(25);
@@ -55,61 +53,5 @@ public class Testing {
 		System.out.println("id:"+user.getId());
 	}
 	
-	@Test
-	public void t3() throws IOException{
-		// 加载全局配置文件
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = Resources.getResourceAsStream(resource);
-		// 创建工厂
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		// 开启session
-		SqlSession sqlSession = sqlSessionFactory.openSession();
-		UserDao userDao = sqlSession.getMapper(UserDao.class);
-		List<User> list = userDao.findAll();
-		System.out.println(list);
-	}
-	
-	@Test
-	public void findUserByID() throws IOException{
-		SqlSession sqlSession = sqlSessionFactory.openSession();
-		UserDao userDao = sqlSession.getMapper(UserDao.class);
-		User user = userDao.findUserByID(2);
-		System.out.println(user);
-	}
-	
-	@Test
-	public void findUserByName() throws IOException{
-		SqlSession sqlSession = sqlSessionFactory.openSession();
-		UserDao userDao = sqlSession.getMapper(UserDao.class);
-		List<User> list = userDao.findUserByName("小");
-		System.out.println(list);
-	}
-	
-	@Test
-	public void insertUser() throws IOException{
-		SqlSession sqlSession = sqlSessionFactory.openSession();
-		UserDao userDao = sqlSession.getMapper(UserDao.class);
-		User user = new User();
-		user.setAge(25);
-		user.setName("小海");
-		user.setAddress("北京延庆");
-		userDao.insertUser(user);
-		sqlSession.commit();
-		System.out.println(user);
-	}
-	
-	@Test
-	public void updateUser() throws IOException{
-		SqlSession sqlSession = sqlSessionFactory.openSession();
-		UserDao userDao = sqlSession.getMapper(UserDao.class);
-		User user = new User();
-		user.setId(12);
-		user.setAge(25);
-		user.setName("聂风");
-		user.setAddress("天下会");
-		userDao.updateUser(user);
-		sqlSession.commit();
-		System.out.println(user);
-	}
 }
 
